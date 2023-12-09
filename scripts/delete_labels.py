1a28b2d7c9b2d4fb1e500b294cd935015acf97d3
Augusto Fotino <75763288+AugustoFKL@users.noreply.github.com>
2023-12-09 10:20:09 -0300

    build: add python scripts to setup and delete repository labels (#3)

---
+++ scripts/delete_labels.py
@@ -0,0 +1,136 @@
+import subprocess
+import sys
+import json
+
+from log import logger
+
+# GitHub User/Organization - Replace with your own.
+user = "AugustoFKL"
+
+
+def remove_label(repo, name):
+    """
+    Remove a label from a GitHub repository.
+
+    Args:
+        repo (str): Repository name to remove the label from.
+        name (str): Name of the label.
+    """
+
+    # Command to remove label using GitHub CLI
+    command = (
+        f"gh api "
+        f"--method DELETE "
+        f"-H \"Accept: application/vnd.github.v3+json\" "
+        f"repos/{user}/{repo}/labels/\"{name}\""
+    )
+    try:
+        subprocess.run(
+            command,
+            check=True,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        logger.debug(f"Label '{name}' removed successfully.")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to remove label '{name}': {e.stdout}, {e.stderr}")
+        sys.exit(1)
+
+
+def get_labels(repo):
+    """
+    Get all labels from a GitHub repository.
+
+    Args:
+        repo (str): Repository name to get labels from.
+
+    Returns:
+        list: List of labels.
+    """
+
+    logger.info(f"Retrieving labels from '{repo}'...")
+
+    command = (
+        f"gh api "
+        f"--method GET "
+        f'-H "Accept: application/vnd.github.v3+json" '
+        f"repos/{user}/{repo}/labels"
+    )
+
+    try:
+        result = subprocess.run(
+            command,
+            check=True,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        logger.debug(f"Labels retrieved successfully.")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to retrieve labels: {e.stdout}")
+        sys.exit(1)
+
+    response = json.loads(result.stdout.decode("utf-8"))
+
+    # The labels are returned in the format:
+    # [
+    #   {
+    #     "id": 208045946,
+    #     "node_id": "MDU6TGFiZWwyMDgwNDU5NDY=",
+    #     "url": "https://api.github.com/repos/octocat/Hello-World/labels/bug",
+    #     "name": "bug",
+    #     "description": "Something isn't working",
+    #     "color": "f29513",
+    #     "default": true
+    #   },
+    #   {
+    #     "id": 208045947,
+    #     "node_id": "MDU6TGFiZWwyMDgwNDU5NDc=",
+    #     "url": "https://api.github.com/repos/octocat/Hello-World/labels/enhancement",
+    #     "name": "enhancement",
+    #     "description": "New feature or request",
+    #     "color": "a2eeef",
+    #     "default": false
+    #   }
+    # ]
+
+    labels = []
+    for item in response:
+        name = item["name"]
+        labels.append(name)
+        logger.debug(f"Label '{name}' retrieved successfully.")
+
+    return labels
+
+
+def main(repo):
+    """
+    Main function to remove all labels from a repository.
+
+    Args:
+        repo (str): Repository name to remove labels from.
+    """
+
+    labels = get_labels(repo)
+
+    for label in labels:
+        remove_label(repo, label)
+
+
+if __name__ == "__main__":
+    logger.info("Starting label setup...")
+
+    # Check if repository name was provided
+    if len(sys.argv) == 1:
+        logger.error("Repository name isn't provided.")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        logger.error(
+            "Too many arguments provided, only the repository name is needed, received {}.".format(
+                sys.argv
+            )
+        )
+        sys.exit(1)
+
+    main(sys.argv[1])
---
+++ scripts/setup-labels.py
@@ -0,0 +1,99 @@
+import random
+import subprocess
+import sys
+
+from log import logger
+
+# GitHub User/Organization - Replace with your own.
+user = "AugustoFKL"
+
+
+def add_label(repo, name, description, color):
+    """
+    Add a label to a GitHub repository.
+
+    Args:
+        repo (str): Repository name to add the label to.
+        name (str): Name of the label.
+        description (str): Description of the label.
+        color (str): Color of the label.
+    """
+
+    # Command to add label using GitHub CLI
+    command = (
+        f"gh api "
+        f"--method POST "
+        f"-H \"Accept: application/vnd.github+json\" "
+        f"repos/{user}/{repo}/labels "
+        f"-f name=\"{name}\" "
+        f"-f description=\"{description}\" "
+        f"-f color=\"{color}\""
+    )
+    try:
+        subprocess.run(command, check=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logger.debug(f"Label '{name}' added successfully.")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Failed to add label '{name}': {e.stdout}")
+        sys.exit(1)
+
+
+# Function to generate a random color
+def generate_random_color():
+    """
+    Generate a random color in hex format, e.g. #FFFFFF.
+    """
+    return format(random.randint(0, 0xFFFFFF), '06X')
+
+
+def main(repo):
+    """
+    Main function.
+
+    Args:
+        repo (str): Repository name to setup labels for.
+    """
+
+    # Labels to be added
+    labels = [
+        {"name": "env: cross-platform", "description": "Affects multiple operating systems."},
+        {"name": "env: linux", "description": "Pertaining to Linux-based systems."},
+        {"name": "env: macos", "description": "Exclusively for macOS-related topics."},
+        {"name": "env: windows", "description": "Windows-specific issues or improvements."},
+        {"name": "type: build", "description": "Changes to the code building process."},
+        {"name": "type: chore", "description": "Routine tasks and project maintenance."},
+        {"name": "type: ci", "description": "Adjustments in CI processes and tools."},
+        {"name": "type: docs", "description": "Exclusively for documentation updates."},
+        {"name": "type: feat", "description": "For new functionalities or significant enhancements."},
+        {"name": "type: fix", "description": "Used to correct and address software defects."},
+        {"name": "type: perf", "description": "Enhances efficiency and speed of existing features."},
+        {"name": "type: refactor", "description": "Internal code improvements without behavior change."},
+        {"name": "type: revert", "description": "Undoing previous code changes."},
+        {"name": "type: style", "description": "For non-functional code style improvements."},
+        {"name": "type: test", "description": "Related to adding or improving test cases."},
+        {"name": "workflow: critical", "description": "High-priority, severe impact issues."},
+        {"name": "workflow: discussion-needed", "description": "Requires collective decision-making."},
+        {"name": "workflow: good-first-issue", "description": "Suitable for new contributors."},
+        {"name": "workflow: help-wanted", "description": "Open call for community contributors."},
+        {"name": "workflow: needs-triage", "description": "New, unassessed issues for review."},
+        {"name": "workflow: up-for-grabs", "description": "Ready for immediate work, not urgent."}
+    ]
+
+    # Add each label to the repository
+    for label in labels:
+        add_label(repo, label["name"], label["description"], generate_random_color())
+
+    logger.info("All labels added successfully.")
+
+
+if __name__ == "__main__":
+    logger.info("Starting label setup...")
+
+    # Check if repository name was provided
+    if len(sys.argv) == 1:
+        logger.error("Repository name isn't provided.")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        logger.error("Too many arguments provided, only the repository name is needed, received {}.".format(sys.argv))
+        sys.exit(1)
+
+    main(sys.argv[1])
