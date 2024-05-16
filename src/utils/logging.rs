d9d76c4fcb30711e54c275a97d691efe0c5c3cde
AugustoFKL <augusto.fotino@proton.me>
2024-05-16 19:39:43 -0300

    chore: (14) Fix linter warnings

--- src/utils/logging.rs
+++ src/utils/logging.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use log4rs::append::console::ConsoleAppender;
-use log4rs::Config;
-use log4rs::config::{Appender, Root};
 use log::LevelFilter;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::config::{Appender, Root};
+use log4rs::Config;
 
 pub fn init() -> Result<()> {
     let stdout = ConsoleAppender::builder().build();
