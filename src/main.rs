f3ab09bf48caea9917b528663343af62e7b3964a
AugustoFKL <augusto.fotino@proton.me>
2024-10-09 17:43:16 -0300

    chore: Cleanup project

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-#![warn(clippy::all, clippy::nursery, clippy::pedantic)]
-
-use log::info;
-
-use crate::utils::logging;
-
-pub(crate) mod utils;
-
 fn main() {
-    logging::init().unwrap();
-
-    info!("Hello, world!");
+    
 }
